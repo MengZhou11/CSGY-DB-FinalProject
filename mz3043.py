82e0ad35070007d50615288ce4b0d93e0918d516
Meng <61612823+MengZhou11@users.noreply.github.com>
2022-12-10 17:44:14 -0500

    Update mz3043.py

--- mz3043.py
+++ mz3043.py
@@ -1,5 +1,25 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import streamlit_authenticator as stauth
 
 st.title('Uber pickups in NYC')
+
+def show_news_for_user():
+    st.write("hi user")
+def show_news_for_admin():
+    st.write("hi admin")
+
+if __name__ == '__main__':
+    st.title("NYU Campus News System")
+    menu = ["News", "Login", "SignUp"] 
+    choice = st.sidebar.selectbox["Menu", menu]
+
+    if choice == "News":
+        st.subheader("NYU Campus News System")
+
+    if choice == "Login":
+        st.subheader("Login Section")
+
+    if choice == "SignUp":
+        st.subheader("Create New Account")
